dda104ad61a6a814e059d8734009406360179aa6
Aidan Au <aidan.au.linkedin@gmail.com>
2024-03-06 07:30:07 -0500

    Finished HomePagePublic (page 2)

--- src/HomePagePublic/HomePagePublic.js
+++ src/HomePagePublic/HomePagePublic.js
@@ -1,7 +1,23 @@
 import React from 'react';
-import {useNavigate} from "react-router-dom";
+import {useHistory} from "react-router-dom";
 
 function HomePagePublic() {
+    const history = useHistory();
+
+    const goToPageLogin = () => {
+        history.push("../Login/Login");
+    }
+
+    return (
+        <div className="HomePagePublic">
+            <h1>Home Page Public View</h1> 
+            <p>Welcome To The Assignment Review App</p> 
+            <button className ="btn" onClick={goToPageLogin}>Login</button>
+        </div>
+    );
+
+
+    
 };
 
 export default HomePagePublic;
