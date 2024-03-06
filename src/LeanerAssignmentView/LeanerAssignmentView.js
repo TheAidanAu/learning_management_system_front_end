5c5e2c93ac9bc84b7149d72f5cd80629dd42bdbb
Aidan Au <aidan.au.linkedin@gmail.com>
2024-03-06 08:10:08 -0500

    Added a helper method of handleSubmit

--- src/LeanerAssignmentView/LeanerAssignmentView.js
+++ src/LeanerAssignmentView/LeanerAssignmentView.js
@@ -4,5 +4,22 @@ export default function LeanerAssignmentView() {
   const [newAssignment, setNewAssignment] = useState("")
   const [assignmentList, setAssignmentList] = useState([])
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    
+    // This updates the assignmentList state varaible by 
+    // adding a newAssignment object to the current assignemtnList
+    setAssignmentList(currentAssignmentList => {
+      return [
+        ...currentAssignmentList,
+        { id: crypto.randomUUID(), title: newAssignment, completed: false
+        }, 
+      ]
+    }
+    )
+    // This empties the text box/field after clicking Submit
+    setNewAssignment("")
+  }
+
 
 }
