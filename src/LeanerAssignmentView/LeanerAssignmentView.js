5aec531b1e86db47bd8d973e66c0ebbdc09674eb
Aidan Au <aidan.au.linkedin@gmail.com>
2024-03-06 07:46:12 -0500

    Started LearnerAssignmentView (page 3)

---
+++ src/LeanerAssignmentView/LeanerAssignmentView.js
@@ -0,0 +1,8 @@
+import { useState } from "react"
+
+export default function LeanerAssignmentView() {
+  const [newAssignment, setNewAssignment] = useState("")
+  const [assignmentList, setAssignmentList] = useState([])
+
+
+}
